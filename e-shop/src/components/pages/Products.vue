b3976027cc5d9d73841aa93ca2b0ceb1f5d13df5
clarklin <clarkms101@gmail.com>
2021-06-21 22:54:05 +0800

    update products page template

--- e-shop/src/components/pages/Products.vue
+++ e-shop/src/components/pages/Products.vue
@@ -1,5 +1,40 @@
 <template>
-  <div>123</div>
+  <div>
+    <div class="text-end mt-4">
+      <button class="btn btn-primary">建立新的產品</button>
+    </div>
+    <table class="table mt-4">
+      <thead>
+        <tr>
+          <th width="120">分類</th>
+          <th>產品名稱</th>
+          <th width="120">原價</th>
+          <th width="120">售價</th>
+          <th width="80">是否啟用</th>
+          <th width="80">編輯</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in products" :key="item.id">
+          <td>{{ item.category }}</td>
+          <td>{{ item.title }}</td>
+          <td class="text-end">
+            {{ item.origin_price }}
+          </td>
+          <td class="text-end">
+            {{ item.price }}
+          </td>
+          <td>
+            <span v-if="item.is_enabled" class="text-success">啟用</span>
+            <span v-else>未啟用</span>
+          </td>
+          <td>
+            <button class="btn btn-outline-primary btn-sm">編輯</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
