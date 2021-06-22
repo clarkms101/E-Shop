2232df24e1e6aa1ffbcf56daa2a4549dcc6ac033
clarklin <clarkms101@gmail.com>
2021-06-22 22:00:14 +0800

    adjust bootstrap version

--- e-shop/src/main.js
+++ e-shop/src/main.js
@@ -1,8 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+// NPM下載的套件
 import Vue from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
+
+// 自行定義的
 import App from "./App";
 import router from "./router";
 
