56ced15d4d49405798cd1c65b1051b775258e8eb
clarklin <clarkms101@gmail.com>
2021-07-04 15:34:36 +0800

    adjust customer orders
    - add product modal

--- e-shop/src/components/pages/CustomerOrders.vue
+++ e-shop/src/components/pages/CustomerOrders.vue
@@ -29,13 +29,88 @@
             </div>
           </div>
           <div class="card-footer d-flex">
-            <button type="button" class="btn btn-outline-secondary btn-sm">
-              <i class="fa fa-spinner fa-spin"></i>
+            <button
+              type="button"
+              class="btn btn-outline-secondary btn-sm"
+              @click="getProduct(item.id)"
+            >
+              <i
+                class="fa fa-spinner fa-spin"
+                v-if="status.loadingItem === item.id"
+              ></i>
               查看更多
             </button>
             <button type="button" class="btn btn-outline-danger btn-sm ml-auto">
-              <i class="fa fa-spinner fa-spin"></i>
+              <i
+                class="fa fa-spinner fa-spin"
+                v-if="status.loadingItem === item.id"
+              ></i>
               加到購物車
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Product Modal (Detail) -->
+    <div
+      class="modal fade"
+      id="productModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog" role="document">
+        <div class="modal-content border-0">
+          <div class="modal-header bg-dark text-white">
+            <h5 class="modal-title" id="exampleModalLabel">
+              <span>{{ product.title }}</span>
+            </h5>
+            <button
+              type="button"
+              class="close"
+              data-dismiss="modal"
+              aria-label="Close"
+            >
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            <img :src="product.imageUrl" class="img-fluid" alt="" />
+            <blockquote class="blockquote mt-3">
+              <p class="mb-0">{{ product.content }}</p>
+              <footer class="blockquote-footer text-right">
+                {{ product.description }}
+              </footer>
+            </blockquote>
+            <div class="d-flex justify-content-between align-items-baseline">
+              <div class="h4" v-if="!product.price">
+                原價 {{ product.origin_price }} 元
+              </div>
+              <del class="h6" v-if="product.price"
+                >原價 {{ product.origin_price }} 元</del
+              >
+              <div class="h4" v-if="product.price">
+                現在只要 {{ product.price }} 元
+              </div>
+            </div>
+            <select
+              name=""
+              class="form-control mt-3"
+              id=""
+              v-model="product.num"
+            >
+              <option value="1">選購 1 件</option>
+            </select>
+          </div>
+          <div class="modal-footer">
+            <div class="text-muted text-nowrap mr-3">
+              小計 <strong>{{ product.num * product.price }}</strong
+              >元
+            </div>
+            <button type="button" class="btn btn-primary">
+              <i class="fa fa-plus"></i>加到購物車
             </button>
           </div>
         </div>
@@ -45,10 +120,16 @@
 </template>
 
 <script>
+import $ from "jquery";
+
 export default {
   data() {
     return {
       products: [],
+      product: {},
+      status: {
+        loadingItem: ""
+      },
       isLoading: false
     };
   },
@@ -61,6 +142,17 @@ export default {
         console.log(response.data);
         vm.isLoading = false;
         vm.products = response.data.products;
+      });
+    },
+    getProduct(id) {
+      const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/product/${id}`;
+      const vm = this;
+      vm.status.loadingItem = id;
+      this.$http.get(url).then(response => {
+        console.log(response.data);
+        vm.product = response.data.product;
+        $("#productModal").modal("show");
+        vm.status.loadingItem = "";
       });
     }
   },
