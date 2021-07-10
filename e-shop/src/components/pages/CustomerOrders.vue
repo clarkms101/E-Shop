0e0bd9281280393064710ef57d098261e1eac76a
clarklin <clarkms101@gmail.com>
2021-07-10 23:12:25 +0800

    adjust customer orders
    add coupon

--- e-shop/src/components/pages/CustomerOrders.vue
+++ e-shop/src/components/pages/CustomerOrders.vue
@@ -137,51 +137,68 @@
     </div>
 
     <!-- shopping cart -->
-    <div>
-      <table class="table mt-4">
-        <thead>
-          <th></th>
-          <th>品名</th>
-          <th>數量</th>
-          <th width="150">單價</th>
-        </thead>
-        <tbody v-if="cart.carts">
-          <tr v-for="item in cart.carts" :key="item.id">
-            <td class="align-middle">
-              <button
-                type="button"
-                class="btn btn-outline-danger btn-sm"
-                @click="removeFromCart(item.id)"
-              >
-                <i class="fa fa-trash-o"></i>
-              </button>
-            </td>
-            <td class="align-middle">
-              {{ item.product.title }}
-              <div v-if="item.coupon" class="text-success">已套用優惠券</div>
-            </td>
-            <td class="align-middle">{{ item.qty }} {{ item.product.unit }}</td>
-            <td class="align-middle text-right">
-              {{ item.final_total | currency }}
-            </td>
-          </tr>
-        </tbody>
-        <tbody v-else>
-          <tr>
-            <td>尚未加入任何產品，快去逛逛吧！</td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colspan="3" class="text-right">總計</td>
-            <td class="text-right">{{ cart.total | currency }}</td>
-          </tr>
-          <tr v-if="cart.final_total !== cart.total">
-            <td colspan="3" class="text-right text-success">折扣價</td>
-            <td class="text-right text-success">{{ cart.final_total }}</td>
-          </tr>
-        </tfoot>
-      </table>
+    <div class="container">
+      <div class="row align-items-center" style="margin:10%">
+
+        <!-- cart -->
+        <table class="table mt-4">
+          <thead>
+            <th></th>
+            <th>品名</th>
+            <th>數量</th>
+            <th width="150">單價</th>
+          </thead>
+          <tbody v-if="cart.carts">
+            <tr v-for="item in cart.carts" :key="item.id">
+              <td class="align-middle">
+                <button
+                  type="button"
+                  class="btn btn-outline-danger btn-sm"
+                  @click="removeFromCart(item.id)"
+                >
+                  <i class="fa fa-trash-o"></i>
+                </button>
+              </td>
+              <td class="align-middle">
+                {{ item.product.title }}
+                <div v-if="item.coupon" class="text-success">已套用優惠券</div>
+              </td>
+              <td class="align-middle">
+                {{ item.qty }} {{ item.product.unit }}
+              </td>
+              <td class="align-middle text-right">
+                {{ item.final_total | currency }}
+              </td>
+            </tr>
+          </tbody>
+          <tbody v-else>
+            <tr>
+              <td>尚未加入任何產品，快去逛逛吧！</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colspan="3" class="text-right">總計</td>
+              <td class="text-right">{{ cart.total | currency }}</td>
+            </tr>
+            <tr v-if="cart.final_total !== cart.total">
+              <td colspan="3" class="text-right text-success">折扣價</td>
+              <td class="text-right text-success">{{ cart.final_total }}</td>
+            </tr>
+          </tfoot>
+        </table>
+
+        <!-- coupon -->
+        <div class="input-group mb-3 input-group-sm">
+          <input type="text" class="form-control" placeholder="請輸入優惠碼" />
+          <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button">
+              套用優惠碼
+            </button>
+          </div>
+        </div>
+
+      </div>
     </div>
   </div>
 </template>
@@ -193,7 +210,7 @@ import Pagination from "../Pagination.vue";
 export default {
   name: "CustomerOrder",
   components: {
-    Pagination,
+    Pagination
   },
   data() {
     return {
@@ -203,28 +220,28 @@ export default {
       pagination: {},
       qty: "",
       status: {
-        loadingItem: "",
+        loadingItem: ""
       },
-      isLoading: false,
+      isLoading: false
     };
   },
   computed: {
     totalPrice() {
       return this.qty * this.product.price;
-    },
+    }
   },
   watch: {
     qty() {
       const qty = this.qty * 1;
       this.product.num = qty;
-    },
+    }
   },
   methods: {
     getProducts(page = 1) {
       const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/products?page=${page}`;
       const vm = this;
       vm.isLoading = true;
-      this.$http.get(url).then((response) => {
+      this.$http.get(url).then(response => {
         console.log(response.data);
         vm.isLoading = false;
         vm.products = response.data.products;
@@ -235,7 +252,7 @@ export default {
       const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/product/${id}`;
       const vm = this;
       vm.status.loadingItem = id;
-      this.$http.get(url).then((response) => {
+      this.$http.get(url).then(response => {
         console.log(response.data);
         vm.product = response.data.product;
         vm.qty = "";
@@ -249,9 +266,9 @@ export default {
       vm.status.loadingItem = id;
       const cart = {
         product_id: id,
-        qty: qty,
+        qty: qty
       };
-      this.$http.post(url, { data: cart }).then((response) => {
+      this.$http.post(url, { data: cart }).then(response => {
         console.log(response.data);
         vm.status.loadingItem = "";
         vm.getCart();
@@ -262,7 +279,7 @@ export default {
       const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart`;
       const vm = this;
       vm.isLoading = true;
-      this.$http.get(url).then((response) => {
+      this.$http.get(url).then(response => {
         console.log(response.data);
         vm.cart = response.data.data;
         vm.isLoading = false;
@@ -271,16 +288,16 @@ export default {
     removeFromCart(id) {
       const url = `${process.env.APIPATH}/api/${process.env.CUSTOMPATH}/cart/${id}`;
       this.isLoading = true;
-      this.$http.delete(url).then((response) => {
+      this.$http.delete(url).then(response => {
         this.$bus.$emit("message:push", response.data.message, "success");
         this.getCart();
         this.isLoading = false;
       });
-    },
+    }
   },
   created() {
     this.getProducts();
     this.getCart();
-  },
+  }
 };
 </script>
