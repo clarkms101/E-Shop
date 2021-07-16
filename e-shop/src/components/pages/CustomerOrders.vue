4c6578fc87daff6ff2aa182c61d203cc2cbbbd5f
clarklin <clarkms101@gmail.com>
2021-07-17 00:21:16 +0800

    adjust customer orders
    - add order form

--- e-shop/src/components/pages/CustomerOrders.vue
+++ e-shop/src/components/pages/CustomerOrders.vue
@@ -137,10 +137,10 @@
     </div>
 
     <!-- shopping cart -->
-    <div class="container">
-      <div class="row align-items-center" style="margin:10%">
-        <!-- cart -->
-        <table class="table mt-4">
+    <div class="my-5 row justify-content-center">
+      <div class="col-md-6">
+        <!-- shopping items -->
+        <table class="table">
           <thead>
             <th></th>
             <th>品名</th>
@@ -209,6 +209,77 @@
         </div>
       </div>
     </div>
+
+    <!-- order -->
+    <div class="my-5 row justify-content-center">
+      <form class="col-md-6">
+        <div class="form-group">
+          <label for="useremail">Email</label>
+          <input
+            type="email"
+            class="form-control"
+            name="email"
+            id="useremail"
+            v-model="form.user.email"
+            placeholder="請輸入 Email"
+            required
+          />
+          <span class="text-danger"></span>
+        </div>
+
+        <div class="form-group">
+          <label for="username">收件人姓名</label>
+          <input
+            type="text"
+            class="form-control"
+            name="name"
+            id="username"
+            v-model="form.user.name"
+            placeholder="輸入姓名"
+          />
+          <span class="text-danger"></span>
+        </div>
+
+        <div class="form-group">
+          <label for="usertel">收件人電話</label>
+          <input
+            type="tel"
+            class="form-control"
+            id="usertel"
+            v-model="form.user.tel"
+            placeholder="請輸入電話"
+          />
+        </div>
+
+        <div class="form-group">
+          <label for="useraddress">收件人地址</label>
+          <input
+            type="text"
+            class="form-control"
+            name="address"
+            id="useraddress"
+            v-model="form.user.address"
+            placeholder="請輸入地址"
+          />
+          <span class="text-danger">地址欄位不得留空</span>
+        </div>
+
+        <div class="form-group">
+          <label for="comment">留言</label>
+          <textarea
+            name=""
+            id="comment"
+            class="form-control"
+            cols="30"
+            rows="10"
+            v-model="form.message"
+          ></textarea>
+        </div>
+        <div class="text-right">
+          <button class="btn btn-danger">送出訂單</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -230,6 +301,15 @@ export default {
       qty: "",
       status: {
         loadingItem: ""
+      },
+      form: {
+        user: {
+          name: "",
+          email: "",
+          tel: "",
+          address: ""
+        },
+        message: ""
       },
       isLoading: false,
       coupon_code: ""
