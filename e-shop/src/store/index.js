ce974c51dd7566140f9ba0cc35cf8a197111ac37
clarklin <clarkms101@gmail.com>
2021-08-08 22:12:01 +0800

    close Vuex strict mode in production environment

--- e-shop/src/store/index.js
+++ e-shop/src/store/index.js
@@ -5,8 +5,8 @@ import axios from "axios";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  // 嚴格模式(state不允許外部直接修改)
-  strict: true,
+  // 嚴格模式(state不允許外部直接修改) (正式環境不啟用嚴格模式，避免效能耗損)
+  strict: process.env.NODE_ENV !== 'production',
   // 資料狀態
   state: {
     isLoading: false,
