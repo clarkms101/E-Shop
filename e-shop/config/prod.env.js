2623395b33999f6047e2d81baaeff38c5f58f556
clarklin <clarkms101@gmail.com>
2021-08-13 21:44:26 +0800

    add Dockerfile

--- e-shop/config/prod.env.js
+++ e-shop/config/prod.env.js
@@ -1,4 +1,6 @@
-'use strict'
+"use strict";
 module.exports = {
-  NODE_ENV: '"production"'
-}
+  NODE_ENV: '"production"',
+  APIPATH: '"https://vue-course-api.hexschool.io"',
+  CUSTOMPATH: '"clarklin"'
+};
