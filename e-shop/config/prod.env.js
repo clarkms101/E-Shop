17b0272c9acc919706bfb2551bd3ab72273900bc
clarklin <clarkms101@gmail.com>
2021-08-30 13:47:46 +0800

    adjust production API path

--- e-shop/config/prod.env.js
+++ e-shop/config/prod.env.js
@@ -1,6 +1,5 @@
 "use strict";
 module.exports = {
   NODE_ENV: '"production"',
-  APIPATH: '"https://vue-course-api.hexschool.io"',
-  CUSTOMPATH: '"clarklin"'
+  APIPATH: '"https://e-shop-backend-clark.herokuapp.com"'
 };
